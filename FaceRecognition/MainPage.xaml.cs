4968bb480ee765234c08a353f154488b89395a76
Agnieszka <agnieszka_szczotka@onet.pl>
2014-05-09 11:54:40 +0200

    poczatekprac

---
+++ FaceRecognition/MainPage.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.Media.MediaProperties;
+using Windows.Media.Capture;
+using Windows.Media;
+using Windows.Storage;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.Storage.Streams;
+
+
+
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace FaceRecognition
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        
+        public MainPage()
+        {
+            this.InitializeComponent();
+            
+        }
+       
+        internal async void btnStartDevice_Click(Object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            CameraCaptureUI dialog = new CameraCaptureUI();
+            Size aspectRatio = new Size(16, 9);
+            dialog.PhotoSettings.CroppedAspectRatio = aspectRatio;
+            StorageFile file = await dialog.CaptureFileAsync(CameraCaptureUIMode.Photo);
+            if (file != null)
+            {
+                BitmapImage bitmapImage = new BitmapImage();
+                using (IRandomAccessStream fileStream = await file.OpenAsync(FileAccessMode.Read))
+                {
+                    bitmapImage.SetSource(fileStream);
+                }
+            }
+            
+        }
+        
+    }
+}
---
+++ FaceRecognition/Memento/Sepia.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab01biometria.imageoperation
+{
+    class Sepia:Visitor
+    {
+        
+        int Waga;
+        image_RGB rgb=null;
+        public Sepia(int Waga){
+            this.Waga = Waga;
+            
+        }
+        public void SepiaAll(image_as_tab image)
+        {
+            image.Accept(this);
+        }
+        public string rob(image_as_tab image) {
+            SepiaAll(image);
+            return opis;
+        }
+        public void Visit(image_RGB rgb)
+        {
+
+            byte[] temp = new byte[rgb.w * rgb.h * 4];
+
+            for (int i = 0; i < rgb.w; i++)
+            {
+                for (int j = 0; j < rgb.h; j++)
+                {
+                    //R=R+2*W, G=G+W, B=B
+
+                    if (rgb.G[i][j] + Waga <= 255)
+                        rgb.G[i][j] = (byte)(rgb.G[i][j] + Waga);
+                    else
+                        rgb.G[i][j] = 255;
+                    if (rgb.R[i][j] + 2 * Waga <= 255)
+                        rgb.R[i][j] = (byte)(rgb.R[i][j] + 2 * Waga);
+                    else
+                        rgb.R[i][j] = 255;
+
+                }
+
+            }
+
+        }
+        public void Visit(image_Gray Grey) {
+            rgb = new image_RGB();
+            byte[] temp = new byte[rgb.w * rgb.h * 4];
+
+            for (int i = 0; i < rgb.w; i++)
+            {
+                for (int j = 0; j < rgb.h; j++)
+                {
+                    //R=R+2*W, G=G+W, B=B
+
+                    if (Grey.Greycanal[i][j] + Waga <= 255)
+                        rgb.G[i][j] = (byte)(Grey.Greycanal[i][j] + Waga);
+                    else
+                        rgb.G[i][j] = 255;
+                    if (Grey.Greycanal[i][j] + 2 * Waga <= 255)
+                        rgb.R[i][j] = (byte)(Grey.Greycanal[i][j] + 2 * Waga);
+                    else
+                        rgb.R[i][j] = 255;
+
+                }
+
+            }
+            
+        }
+    }
+}
